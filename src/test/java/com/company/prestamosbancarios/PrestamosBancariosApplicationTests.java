a49685eef2e6d66075ada5904b5e936646c1cd0b
Oscar Rodriguez <cokinho007@hotmail.com>
2026-02-24 12:22:32 -0600

    Prestamos Bancarios

---
+++ src/test/java/com/company/prestamosbancarios/PrestamosBancariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.prestamosbancarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamosBancariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
